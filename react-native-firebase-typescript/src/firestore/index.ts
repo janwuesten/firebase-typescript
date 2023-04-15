52b57955d3e0e9f9d743389f669be23c05c0120f
Jan Wüsten <42694155+janwuesten@users.noreply.github.com>
2023-04-15 15:10:47 +0200

    Fixed missing DocumentFactory export

--- react-native-firebase-typescript/src/firestore/index.ts
+++ react-native-firebase-typescript/src/firestore/index.ts
@@ -2,5 +2,5 @@ export * from "./types/DocumentTypes"
 export * from "./classes/DocumentClass"
 export * from "./classes/DocumentMap"
 export * from "./classes/DocumentParser"
-
+export * from "./classes/DocumentFactory"
 export * from "./classes/DocumentBatch"
