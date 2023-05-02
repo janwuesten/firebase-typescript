0f34b3998996b9d4e0aafebb5e13da26c281601b
Jan Wüsten <42694155+janwuesten@users.noreply.github.com>
2023-05-02 19:49:10 +0200

    Added tests

---
+++ firebase-admin-typescript/test/basic.test.ts
@@ -0,0 +1,78 @@
+import { expect, test } from "@jest/globals"
+import { credential } from "firebase-admin"
+import { initializeApp } from "firebase-admin/app"
+import { getFirestore } from "firebase-admin/firestore"
+import { DocumentBatch, DocumentClass, DocumentClassDefineProps, DocumentFactory } from "../src"
+
+initializeApp({
+    credential: credential.cert("../serviceAccountKey.json")
+})
+
+class Country extends DocumentClass {
+    name: string = ""
+    code: string = ""
+
+    definition({ define, defineCollection }: DocumentClassDefineProps): void {
+        define("name", "string")
+        define("code", "string")
+
+        defineCollection(() => getFirestore().collection("country"))
+    }
+}
+
+let countryID = "";
+
+test("adding documents", async () => {
+    const country = new Country()
+    country.code = "DK"
+    country.name = "Denmark"
+    expect(country.id).toBeFalsy()
+    await country.add()
+    expect(country.id).toBeTruthy()
+    countryID = country.id
+})
+test("getting documents", async () => {
+    const country = new Country(countryID)
+    expect(await country.get()).toBe(true)
+    expect(country.id).toBe(countryID)
+    expect(country.name).toBe("Denmark")
+    expect(country.code).toBe("DK")
+})
+test("updating documents", async () => {
+    const country = new Country(countryID)
+    expect(await country.get()).toBe(true)
+    expect(country.id).toBe(countryID)
+    expect(country.name).toBe("Denmark")
+    expect(country.code).toBe("DK")
+    country.name = "Germany"
+    country.code = "DE"
+    await country.update()
+})
+test("setting documents", async () => {
+    const country = new Country(countryID)
+    expect(await country.get()).toBe(true)
+    expect(country.id).toBe(countryID)
+    expect(country.name).toBe("Germany")
+    expect(country.code).toBe("DE")
+    country.name = "Denmark"
+    country.code = "DK"
+    await country.set()
+    expect(country.id).toBe(countryID)
+})
+test("deleting documents", async () => {
+    const country = new Country(countryID)
+    expect(await country.get()).toBe(true)
+    expect(country.id).toBe(countryID)
+    expect(country.name).toBe("Denmark")
+    expect(country.code).toBe("DK")
+    await country.delete()
+    expect(await new Country(countryID).get()).toBe(false)
+})
+test("clean", async () => {
+    const countries = await new DocumentFactory(() => new Country).getDocs(getFirestore().collection("country"))
+    const batch = new DocumentBatch()
+    for (const country of countries) {
+        await batch.delete(country)
+    }
+    await batch.commit()
+})
---
+++ firebase-admin-typescript/test/types.test.ts
@@ -0,0 +1,115 @@
+import { expect, test } from "@jest/globals"
+import { credential } from "firebase-admin"
+import { initializeApp } from "firebase-admin/app"
+import { getFirestore, GeoPoint} from "firebase-admin/firestore"
+import { DocumentBatch, DocumentClass, DocumentClassDefineProps, DocumentFactory, DocumentMap, DocumentMapDefineProps, Mappable } from "../src"
+
+initializeApp({
+    credential: credential.cert("../serviceAccountKey.json")
+})
+
+class TypeTestMap extends DocumentMap {
+    string: string = ""
+
+    definition({ define }: DocumentMapDefineProps): void {
+        define("string", "string")
+    }
+}
+class TypeTest extends DocumentClass {
+    string: string = ""
+    number: number = 0
+    boolean: boolean = false
+    geopoint: GeoPoint = new GeoPoint(0, 0)
+    stringArray: string[] = []
+    map: TypeTestMap = new TypeTestMap()
+    mapArray: TypeTestMap[] = []
+    mappable: Mappable<TypeTestMap> = new Mappable()
+    timestamp: Date = new Date()
+
+    definition({ define, defineCollection }: DocumentClassDefineProps): void {
+        define("string", "string")
+        define("number", "string")
+        define("population", "number")
+        define("boolean", "boolean")
+        define("geopoint", "geopoint")
+        define("stringArray", "array")
+        define("map", "map").defineMap(() => new TypeTestMap())
+        define("mapArray", "mapArray").defineMap(() => new TypeTestMap())
+        define("mappable", "mappable").defineMap(() => new TypeTestMap())
+        define("timestamp", "timestamp")
+
+        defineCollection(() => getFirestore().collection("typetest"))
+    }
+}
+const timestamp = new Date()
+let typeTestID = ""
+test("setting types", async () => {
+    const typeTest = new TypeTest()
+    typeTest.string = "TEST STRING"
+    typeTest.number = 10
+    typeTest.boolean = true
+    typeTest.geopoint = new GeoPoint(10, 10)
+    typeTest.stringArray = [
+        "Test 1",
+        "Test 2",
+        "Test 3"
+    ]
+    const ttm1 = new TypeTestMap()
+    ttm1.string = "TTM1"
+    typeTest.map = ttm1
+
+    const ttm2 = new TypeTestMap()
+    ttm2.string = "TTM2"
+    const ttm3 = new TypeTestMap()
+    ttm3.string = "TTM3"
+    typeTest.mapArray = [
+        ttm1,
+        ttm2
+    ]
+    typeTest.mapArray.push(ttm3)
+
+    typeTest.mappable.set("ttm1", ttm1)
+    typeTest.mappable.set("ttm2", ttm2)
+    typeTest.mappable.set("ttm3", ttm3)
+
+    typeTest.timestamp = timestamp
+
+    await typeTest.add()
+    typeTestID = typeTest.id
+    expect(typeTestID).toBeTruthy()
+})
+test("getting types", async () => {
+    const typeTest = new TypeTest(typeTestID)
+    expect(await typeTest.get()).toBe(true)
+    expect(typeTest.string).toBe("TEST STRING")
+    expect(typeTest.number).toBe(10)
+    expect(typeTest.boolean).toBe(true)
+    expect(typeTest.geopoint.latitude).toBe(new GeoPoint(10, 10).latitude)
+    expect(typeTest.geopoint.longitude).toBe(new GeoPoint(10, 10).longitude)
+    expect(typeTest.stringArray.length).toBe(3)
+    expect(typeTest.stringArray[0]).toBe("Test 1")
+    expect(typeTest.stringArray[1]).toBe("Test 2")
+    expect(typeTest.stringArray[2]).toBe("Test 3")
+    expect(typeTest.map).toBeTruthy()
+    expect(typeTest.map.string).toBe("TTM1")
+    expect(typeTest.mapArray.length).toBe(3)
+    expect(typeTest.mapArray[0].string).toBe("TTM1")
+    expect(typeTest.mapArray[1].string).toBe("TTM2")
+    expect(typeTest.mapArray[2].string).toBe("TTM3")
+    expect(typeTest.mappable.size).toBe(3)
+    expect(typeTest.mappable.get("ttm1")).toBeTruthy()
+    expect(typeTest.mappable.get("ttm2")).toBeTruthy()
+    expect(typeTest.mappable.get("ttm3")).toBeTruthy()
+    expect(typeTest.mappable.get("ttm1")!.string).toBe("TTM1")
+    expect(typeTest.mappable.get("ttm2")!.string).toBe("TTM2")
+    expect(typeTest.mappable.get("ttm3")!.string).toBe("TTM3")
+    expect(typeTest.timestamp.getTime()).toBe(timestamp.getTime())
+})
+test("clean", async () => {
+    const typeTests = await new DocumentFactory(() => new TypeTest).getDocs(getFirestore().collection("typetest"))
+    const batch = new DocumentBatch()
+    for (const typeTest of typeTests) {
+        await batch.delete(typeTest)
+    }
+    await batch.commit()
+})
