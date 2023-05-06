f0005973415ed7a424404dd32908528fffe75986
Jan Wüsten <42694155+janwuesten@users.noreply.github.com>
2023-05-06 10:33:12 +0200

    Fixed mappable asArray and keysAsArray

--- firebase-admin-typescript/src/firestore/classes/Mappable.ts
+++ firebase-admin-typescript/src/firestore/classes/Mappable.ts
@@ -1,10 +1,9 @@
 export class Mappable<T> extends Map<string, T> {
-    private _map = new Map<string, T>
 
     asArray(): T[] {
-        return [...this._map.values()]
+        return [...this.values()]
     }
     keysAsArray(): string[] {
-        return [...this._map.keys()]
+        return [...this.keys()]
     }
 }
