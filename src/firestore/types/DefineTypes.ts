9025b5cc66681a4dde52f70fd9a3a9cec9a618df
Jan Wüsten <42694155+janwuesten@users.noreply.github.com>
2023-11-24 12:53:43 +0100

    Fixed non generic error

--- src/firestore/types/DefineTypes.ts
+++ src/firestore/types/DefineTypes.ts
@@ -1,10 +1,10 @@
 import { DocumentParserDefinition } from "../classes/DocumentParser"
 import { FieldType } from "./DocumentTypes"
-import { CollectionRefResult, DocumentClassHandler } from "../classes/DocumentClassHandler"
-import { CollectionReference, DocumentReference } from "./FirestoreTypes"
+import { DocumentClassHandler } from "../classes/DocumentClassHandler"
+import { CollectionReference } from "./FirestoreTypes"
 
 export type DocumentDefine = <T>(field: string, type: FieldType, defaultValue?: T) => DocumentParserDefinition<T>
 export type CollectionDefine = (collection: () => CollectionReference) => void
-export type HandlerDefine = (handler: DocumentClassHandler<DocumentReference, CollectionRefResult>) => void
+export type HandlerDefine = (handler: DocumentClassHandler) => void
 export type EventDefineCallback = (() => void) | (() => Promise<void>)
 export type EventDefine = (event: "beforeWrite" | "afterWrite" | "beforeRead" | "afterRead", callback: EventDefineCallback) => void
